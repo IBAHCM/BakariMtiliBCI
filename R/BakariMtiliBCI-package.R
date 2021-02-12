70650b280f84205640f409e5555a3d37b0ecbaab
Bakari Mtili <2500251M@student.gla.ac.uk>
2021-02-12 11:48:54 +0300

    Further commits

---
+++ R/BakariMtiliBCI-package.R
@@ -0,0 +1,16 @@
+#' BakariMtiliBCI
+#'
+#' \code{BakariMtiliSeries03} is a package developed as part of reproducible programming in R.
+#'
+#'  The package contains simple biodiversity data and simulation models.
+#'
+#' @author
+#' Bakari Mtili <2500251M@student.gla.ac.uk> (maintainer)
+#'
+#' @name BakariMtiliBCI-package
+#' @aliases BakariMtiliBCI
+#' @docType package
+#'
+#' @import ggplot2
+#'
+NULL
