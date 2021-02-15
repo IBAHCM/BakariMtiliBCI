dc601d3ff5f2e30f1b04d4bd6c9fd2e2cbf452ea
Bakari Mtili <2500251M@student.gla.ac.uk>
2021-02-15 12:51:41 +0300

    Added tittle

--- demo/diversity-of-BCI.R
+++ demo/diversity-of-BCI.R
@@ -1,4 +1,17 @@
-library(BakariMtiliBCI)
+#' ---
+#' title: "Biodiversity BCI data"
+#' subtitle: "Manipulation of the BCI biodiversity data."
+#' author: "Bakari Mtili"
+#' date: '`r format(Sys.Date(), "%B %d %Y")`'
+#' output: html_document
+#' ---
+
+# Call the required libraries
+library(BakariMtiliBCI) # all analysis scripts will start with this library
+library(rdiversity)
+library(magrittr)
+library(vegan)
+
 
 # Make a copy of the taxonomy as a data frame
 taxonomy <- as.data.frame(bci_taxa)
@@ -7,21 +20,26 @@ taxonomy <- as.data.frame(bci_taxa)
 rownames(taxonomy) <- taxonomy$GenusSpecies
 taxonomy <- taxonomy[rownames(bci_2010),]
 
-library(rdiversity)
+# Create an object which will be used for analyses of diversity
 meta <- metacommunity(bci_2010)
+
+# Calculate the gamma diversity using meta_gamma() in rdiversity package
 results <- meta_gamma(meta, qs = seq(from = 0, to = 5))
+
+# Plot the results
 plot(diversity ~ q, type = "l", data = results)
 
-library(magrittr)
+# Create a distance matrix from a taxonomy
 taxSim <- taxonomy %>%
   tax2dist(c(GenusSpecies=0, Genus=1, Family=2, Other=3)) %>%
-  dist2sim("linear")
+  dist2sim("linear") # dist2sim creates similarity matrix from the distance matrix
 metatax <- metacommunity(bci_2010, taxSim)
 results4tax <- meta_gamma(metatax, qs = seq(from = 0, to = 5))
 lines(diversity ~ q, col=2, data = results4tax)
 
-library(vegan)
+
 # Swap rows and columns
 abundances <- t(bci_2010)
+
 # Calculate species richness (gamma diversity)
 specnumber(abundances, groups = 1)
